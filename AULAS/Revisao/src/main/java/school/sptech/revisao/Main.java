02a28d357af0dc1dc90751c39c747c46bee3eaa8
unknown <paulo.santosfo@sptech.school>
2022-09-29 19:51:41 -0300

    aula 29/09

---
+++ AULAS/Revisao/src/main/java/school/sptech/revisao/Main.java
@@ -0,0 +1,15 @@
+
+package school.sptech.revisao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> nomes = new ArrayList();
+        
+        nomes.add("Diego");
+        
+        System.out.println(nomes);
+    }
+}
